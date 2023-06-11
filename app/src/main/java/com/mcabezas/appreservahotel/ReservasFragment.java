2e85a1b06fd48e5fc7361464f656fe5528b49289
ravilaUPC21 <u201000444@upc.edu.pe>
2023-06-11 17:33:42 -0500

    Canbios Nuevos

--- app/src/main/java/com/mcabezas/appreservahotel/ReservasFragment.java
+++ app/src/main/java/com/mcabezas/appreservahotel/ReservasFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -44,6 +45,7 @@ public class ReservasFragment extends Fragment {
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
+
     }
 
     @Override
@@ -61,4 +63,6 @@ public class ReservasFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_reservas, container, false);
     }
+
 }
+

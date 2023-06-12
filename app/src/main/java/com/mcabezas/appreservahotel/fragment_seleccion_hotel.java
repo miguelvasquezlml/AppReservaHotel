4587a1d18984805406de0346745398d0a04f5b44
droman2784 <droman2784@hotmail.com>
2023-06-11 21:59:37 -0500

    Cambios pruebas 6

--- app/src/main/java/com/mcabezas/appreservahotel/fragment_seleccion_hotel.java
+++ app/src/main/java/com/mcabezas/appreservahotel/fragment_seleccion_hotel.java
@@ -15,7 +15,7 @@ import android.view.ViewGroup;
  */
 public class fragment_seleccion_hotel extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
+    // TODO: Rename parameter arguments, choose names that matc
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
